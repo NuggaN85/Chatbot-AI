7752d661751b7ff6f2b0cd5cc223cf61a85ffff3
Rose Ludovic <NuggaN85@users.noreply.github.com>
2023-03-07 12:17:03 +0100

    Create Chatbot-AI-wordpress.php

---
+++ wordpress/Chatbot-AI-wordpress.php
@@ -0,0 +1,25 @@
+function chat_with_gpt_shortcode() {
+    return '<div id="chat_with_gpt"></div>';
+}
+add_shortcode( 'chat_with_gpt', 'chat_with_gpt_shortcode' );
+
+function chat_with_gpt_enqueue_scripts() {
+    wp_enqueue_script( 'chat_with_gpt', 'https://cdn.openai.com/chat-widget/widget.prod.js', array(), false, true );
+}
+add_action( 'wp_enqueue_scripts', 'chat_with_gpt_enqueue_scripts' );
+
+function chat_with_gpt_init_script() {
+    ?>
+    <script>
+        window.addEventListener("DOMContentLoaded", function () {
+            chatWidget.init({
+                apiKey: "YOUR_API_KEY",
+                model: "davinci",
+                placeholderText: "Posez votre question ici",
+                title: "Chat with GPT"
+            })
+        });
+    </script>
+    <?php
+}
+add_action( 'wp_footer', 'chat_with_gpt_init_script' );
