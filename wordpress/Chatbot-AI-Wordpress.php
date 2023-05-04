dcfd40d9fc1a075a077f9e7ecc75c6a68ffa700e
Rose Ludovic <NuggaN85@users.noreply.github.com>
2023-05-04 22:39:43 +0200

    Update Chatbot-AI-Wordpress.php

--- wordpress/Chatbot-AI-Wordpress.php
+++ wordpress/Chatbot-AI-Wordpress.php
@@ -1,13 +1,16 @@
+// Crée le shortcode pour le widget de chatGPT
 function chat_with_gpt_shortcode() {
     return '<div id="chat_with_gpt"></div>';
 }
-add_shortcode( 'chat_with_gpt', 'chat_with_gpt_shortcode' );
+add_shortcode('chat_with_gpt', 'chat_with_gpt_shortcode');
 
+// Enregistre et charge les scripts nécessaires pour le widget de chatGPT
 function chat_with_gpt_enqueue_scripts() {
-    wp_enqueue_script( 'chat_with_gpt', 'https://cdn.openai.com/chat-widget/widget.prod.js', array(), false, true );
+    wp_enqueue_script('chat_with_gpt', 'https://cdn.openai.com/chat-widget/widget.prod.js', array(), false, true);
 }
-add_action( 'wp_enqueue_scripts', 'chat_with_gpt_enqueue_scripts' );
+add_action('wp_enqueue_scripts', 'chat_with_gpt_enqueue_scripts');
 
+// Initialise le script de chat GPT avec les paramètres spécifiés
 function chat_with_gpt_init_script() {
     ?>
     <script>
@@ -22,5 +25,4 @@ function chat_with_gpt_init_script() {
     </script>
     <?php
 }
-add_action( 'wp_footer', 'chat_with_gpt_init_script' );
-
+add_action('wp_footer', 'chat_with_gpt_init_script');
